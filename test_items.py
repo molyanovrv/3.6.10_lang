14e3af02ed508eac9c38ac1e0b8626a34d2179ee
my name <molyanovrv@gmail.com>
2023-02-05 13:30:41 +0300

    well done

--- test_items.py
+++ test_items.py
@@ -1,7 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
+import time
+
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_item(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    try:
+        find = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    except NoSuchElementException: 
+        print("no such element")
+        find = None
+    #time.sleep(20)
+    assert find is not None, "no such element"
